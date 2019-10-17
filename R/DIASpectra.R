59dc90fd8fd2999302dd29c90d1ee18cf0e9c3ef
judechang <zhangyanggang1@126.com>
2019-10-17 16:56:29 +0800

    add label

--- R/DIASpectra.R
+++ R/DIASpectra.R
@@ -60,29 +60,33 @@ setMethod(
     info.all <- lapply(seq.pg, function(idx.pg) {
       idx.ms1.raw <- idx.ms1 <- which(scantime.ms1 >= rt.info[idx.pg, 'rtmin'] &
                                         scantime.ms1 <= rt.info[idx.pg, 'rtmax'])
+      cat("1")
       idx.ms1.ext.raw <- idx.ms1.ext <- which(scantime.ms1 >= rt.info[idx.pg, 'rtmin.ext'] &
                                                 scantime.ms1 <= rt.info[idx.pg, 'rtmax.ext'])
+      cat('2')
       mz <- pg.raw[[idx.pg]][idx.smp, 'mz']
       mz.max <- PpmRange(ref = mz, ppm = 25)[2]
-
+      cat('3')
       mz.range <- as.numeric(pg.raw[[idx.pg]][idx.smp, c('mzmin', 'mzmax')])
       idx.swath <- GetIntervalIndex(mz, info.swath$window)
       idx.ms2 <- unname(sapply(scanidx.match[as.character(idx.ms1)], `[`, idx.swath))
       idx.ms2.ext <- unname(sapply(scanidx.match[as.character(idx.ms1.ext)], `[`, idx.swath))
-
+      cat('4')
       eic.ms1.pre <- extractEIC(scan.ms1[idx.ms1.ext],
                                 matrix(mz.range, ncol = 2),
                                 mz)[[1]]
+      cat('5')
       peaks.ms1.ext <- cbind('scanidx' = idx.ms1.ext,
                              'rt' = scantime.ms1[idx.ms1.ext],
                              'mz' = eic.ms1.pre$mz,
                              'intensity' = eic.ms1.pre$intensity)
+      cat('6')
       is.keep <- idx.ms1.ext %in% idx.ms1
       peak.ms1 <- peaks.ms1.ext[is.keep, , drop = FALSE]
-
+      cat('7')
       peak.ms1.ext.smooth.raw <- peak.ms1.ext.smooth <- SmoothLoess(peaks.ms1.ext, span = 0.1)
       peak.ms1.smooth.raw <- peak.ms1.smooth <- peak.ms1.ext.smooth[is.keep, , drop = FALSE]
-
+      cat('8')
       idx.na <- which(is.na(idx.ms2.ext))
       if (length(idx.na) > 0) {
         idx.ms2.ext <- idx.ms2.ext[-idx.na]
@@ -97,22 +101,24 @@ setMethod(
           idx.ms1.ext <-idx.ms1.ext[-idx.ms1.na]
         }
       }
+      cat('9')
 
       if (all(peak.ms1.smooth[, 'intensity.s'] == 0)) {
         return(NULL)
       }
 
       idx.apex.ms1 <- which(idx.ms1.ext == idx.ms1[which.max(peak.ms1.smooth[,'intensity.s'])])
-
+      cat('10')
       rt.apex.ms1 <- peak.ms1.ext.smooth[idx.apex.ms1, 'rt']
       if (nrow(peak.ms1.smooth) < 4) {
         return(NULL)
       }
-
+      cat('11')
       spec.exp.ext <- scan.ms2[idx.ms2.ext]
       spec.apex <- spec.exp.ext[[idx.apex.ms1]]
       idx.ms2.intfilter <- which(spec.apex[,2] >= int.filter &
                                    spec.apex[, 1] <= mz.max)
+      cat('12')
       mz.ms2 <- spec.exp.ext[[idx.apex.ms1]][idx.ms2.intfilter, 1]
       if (length(mz.ms2) == 0) {
         return(NULL)
@@ -120,14 +126,15 @@ setMethod(
       mz.ms2.range <- t(sapply(mz.ms2, function(mz) {
         PpmRange(mz, ppm.ms2.mtp)
       }))
-
+      cat('13')
       eic.ms2.pre <- extractEIC(spec.exp.ext, mz.ms2.range, mz.ms2)
+      cat('14')
       ms2.eic.ext <- lapply(eic.ms2.pre, function(spec) {
         cbind('scanidx' = idx.ms2.ext,
               'rt' = scantime.ms2[idx.ms2.ext],
               do.call(cbind, spec))
       })
-
+      cat('15')
       range.pk.ms1 <- range(which(is.keep))
       info.pk.ms1 <- data.frame('lb' = range.pk.ms1[1],
                                 'apex' = idx.apex.ms1,
@@ -137,6 +144,7 @@ setMethod(
                                 'simp' = FALSE)
       rownames(info.pk.ms1) <- 0
       nm.smp <- gsub('(?i)\\.mzxml', '', basename(fp.smp))
+      cat("16")
       spec.decon <- DecoSpectra(idx.pg,nm.smp, ms2.eic.ext, peak.ms1.smooth,
                                  length(idx.ms2.ext),
                                  idx.apex.eic = idx.apex.ms1,
@@ -147,7 +155,7 @@ setMethod(
                                  is.dec.all = is.dec.all,
                                  is.dec.smoothed = is.dec.smoothed)
 
-
+    cat('17')
       spec.decon
     })
     obj@spectra <- info.all
